d2845fb9febc32598264ed01bb8a9c895b0bef94
Cyrus Beer <cbeer@itx.com>
2017-03-14 22:39:28 -0400

    Connected order items to preview

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,8 @@ import { NavController, Platform, ModalController } from 'ionic-angular';
 
 import { OrderModal } from '../../modals/order/order';
 
+import _ from "lodash";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -15,6 +17,29 @@ export class HomePage {
 	showOrderPreview: boolean = true;
 	showTotalPreview: boolean = true;
 
+	preview() {
+
+		this.state.orderInProgress = {
+			items: [],
+			pickupLocation: null,
+			totalCost: 0
+		};
+		var self = this;
+		_.each(this.state.foodCategories, function(foodCategory) {
+			_.each(foodCategory.items, function(item) {
+				if (item.quantity > 0) {
+					self.state.orderInProgress.items.push(_.cloneDeep(item));
+					self.state.orderInProgress.totalCost += (item.quantity * item.unitCost);
+				}
+			});
+		});
+
+		_.each(this.state.pickupLocations, function(loc) {
+			if (loc.selected === true) {
+				self.state.orderInProgress.pickupLocation = _.cloneDeep(loc);
+			}
+		});
+	}
 	toggle(foodCategory: any) {
 		foodCategory.expanded = !foodCategory.expanded;
 	}
@@ -24,11 +49,13 @@ export class HomePage {
 		let modal = this.modalCtrl.create(OrderModal);
 		modal.present();
 	}
-	add(item, slidingItem) {
+	add(category, item, slidingItem) {
+		category.amt = category.amt + 1;
 		item.quantity = item.quantity + 1;
 		slidingItem.close();
 	}
-	subtract(item, slidingItem) {
+	subtract(category, item, slidingItem) {
+		category.amt = category.amt - 1;
 		item.quantity = item.quantity - 1;
 		slidingItem.close();
 	}
@@ -37,171 +64,348 @@ export class HomePage {
 		this.isAndroid = platform.is('android');
 	}
 
-	foodCategories = [
-		{
-			label: 'Bread',
-			image: 'bread.jpeg',
-			amt: 2,
-			expanded: false,
+
+	state = {
+		account: {
+			firstName: 'Cyrus',
+			lastName: 'Beer',
+			balance: 220
+		},
+		pickupLocations: [
+			{
+				id: 1,
+				name: 'Seacoast Waldorf School',
+				abbreviatedName: 'Waldorf',
+				address: '403 Harold Dow Highway (Route 236)',
+				city: 'Eliot',
+				stateCode: 'ME',
+				zipCode: '03903',
+				lat: 43.1412688,
+				lng: -70.7883459,
+				nextPickup: 'Tues May 18, 3-6pm',
+				pickupStartTime: '2017-05-18 3pm',
+				pickupEndTime: '2017-05-18 6pm',
+				available: false,
+				unavailableReason: 'past_order_cutoff',
+				selected: false
+			},
+			{
+				id: 2,
+				name: 'Little Harbour School',
+				abbreviatedName: 'Little Harbour',
+				address: '50 Clough Drive',
+				city: 'Portsmouth',
+				stateCode: 'NH',
+				zipCode: '03801',
+				lat: 43.0667763,
+				lng: -70.7541994,
+				nextPickup: 'Fri May 21, 3-6pm',
+				pickupStartTime: '2017-05-18 3pm',
+				pickupEndTime: '2017-05-18 6pm',
+				available: true,
+				unavailableReason: null,
+				selected: false
+			}
+		],
+		outstandingOrder: {
 			items: [
 				{
 					name: 'Oma\'s Best',
-					quantity: 1,
-					quantityLabel: 'loaf',
-					quantityLabelPlural: 'loaves'
-				},
-				{
-					name: 'Sourdough',
 					quantity: 0,
-					quantityLabel: 'loaf',
-					quantityLabelPlural: 'loaves'
-				},
-				{
-					name: 'Spent Grain',
-					quantity: 1,
-					quantityLabel: 'loaf',
-					quantityLabelPlural: 'loaves'
+					unitLabelSingular: 'loaf',
+					unitLabelPlural: 'loaves',
+					unitCost: 8,
+					description: 'A traditional Danish / northern German-style pan loaf with a complex hearty flavor.',
+					ingredients: 'Organic dark rye flour, organic whole spelt flour, water, organic sunflower seeds, organic flax seeds, organic sesame seeds, sea salt',
+					producer: 'Juniper Cottage Bake Shop'
 				}
-			]
+			],
+			pickup: {
+				id: 1,
+				name: 'Seacoast Waldorf School',
+				abbreviatedName: 'Waldorf',
+				address: '403 Harold Dow Highway (Route 236)',
+				city: 'Eliot',
+				stateCode: 'ME',
+				zipCode: '03903',
+				lat: 43.1412688,
+				lng: -70.7883459,
+				nextPickup: 'Tues May 18, 3-6pm',
+				startTime: '2017-05-18 3pm',
+				endTime: '2017-05-18 6pm'
+			},
+			totalCost: 20
+		},
+		standingOrder: {
+			items: [
+				{
+					id: 1,
+					quantity: 1
+				}
+			],
+			pickup: {
+				id: 1,
+				reminder: true
+			},
+			totalCost: 20
+		},
+		orderInProgress: {
+			items: [],
+			pickupLocation: null,
+			totalCost: 0
+		},
+		foodCategories: [
+			{
+				id: 1,
+				label: 'Bread',
+				image: 'bread.jpeg',
+				amt: 0,
+				expanded: false,
+				items: [
+					{
+						id: 1,
+						name: 'Oma\'s Best',
+						quantity: 0,
+						unitLabelSingular: 'loaf',
+						unitLabelPlural: 'loaves',
+						unitCost: 8,
+						description: 'A traditional Danish / northern German-style pan loaf with a complex hearty flavor.',
+						ingredients: 'Organic dark rye flour, organic whole spelt flour, water, organic sunflower seeds, organic flax seeds, organic sesame seeds, sea salt',
+						producer: 'Juniper Cottage Bake Shop'
+					},
+					{
+						id: 2,
+						name: 'Sourdough',
+						quantity: 0,
+						unitLabelSingular: 'loaf',
+						unitLabelPlural: 'loaves',
+						unitCost: 8,
+						description: '',
+						ingredients: '',
+						producer: 'Juniper Cottage Bake Shop'
+					},
+					{
+						id: 3,
+						name: 'Spent Grain',
+						quantity: 0,
+						unitLabelSingular: 'loaf',
+						unitLabelPlural: 'loaves',
+						unitCost: 8,
+						description: '',
+						ingredients: '',
+						producer: 'Juniper Cottage Bake Shop'
+					}
+				]
+			},
+			{
+				id: 2,
+				label: 'Dairy / Eggs',
+				image: 'dairy.jpeg',
+				amt: 0,
+				expanded: false,
+				items: [
+					{
+						id: 4,
+						name: 'Milk',
+						quantity: 0,
+						unitLabelSingular: 'gallon',
+						unitLabelPlural: 'gallons',
+						unitCost: 5,
+						description: '',
+						ingredients: '',
+						producer: ''
+					},
+					{
+						id: 5,
+						name: 'Eggs',
+						quantity: 0,
+						unitLabelSingular: 'dozen',
+						unitLabelPlural: 'dozen',
+						unitCost: 6,
+						description: '',
+						ingredients: '',
+						producer: ''
+					}
+				]
+			},
+			{
+				id: 3,
+				label: 'Dry Goods',
+				image: 'drygoods.jpeg',
+				amt: 0,
+				expanded: false,
+				items: [
+					{
+						id: 6,
+						name: 'Rolled Oats',
+						quantity: 0,
+						unitLabelSingular: 'bag',
+						unitLabelPlural: 'bags',
+						unitCost: 8,
+						description: '',
+						ingredients: '',
+						producer: ''
+					},
+					{
+						id: 7,
+						name: 'Brown Rice',
+						quantity: 0,
+						unitLabelSingular: 'bag',
+						unitLabelPlural: 'bags',
+						unitCost: 8,
+						description: '',
+						ingredients: '',
+						producer: ''
+					}
 
-		},
-		{
-			label: 'Dairy / Eggs',
-			image: 'dairy.jpeg',
-			amt: 1,
-			expanded: false,
-			items: [
-				{
-					name: 'Milk',
-					quantity: 1,
-					quantityLabel: 'gallon',
-					quantityLabelPlural: 'gallons'
-				},
-				{
-					name: 'Eggs',
-					quantity: 0,
-					quantityLabel: 'dozen',
-					quantityLabelPlural: 'dozen'
-				}
+				]
+			},
+			{
+				id: 4,
+				label: 'Ferments',
+				image: 'ferments.jpeg',
+				amt: 0,
+				expanded: false,
+				items: [
+					{
+						id: 8,
+						name: 'Sauerkraut',
+						quantity: 0,
+						unitLabelSingular: 'jar',
+						unitLabelPlural: 'jars',
+						unitCost: 8,
+						description: '',
+						ingredients: '',
+						producer: ''
+					},
+					{
+						id: 9,
+						name: 'Ruby Kraut',
+						quantity: 0,
+						unitLabelSingular: 'jar',
+						unitLabelPlural: 'jars',
+						unitCost: 8,
+						description: '',
+						ingredients: '',
+						producer: ''
+					}
 
-			]
-		},
-		{
-			label: 'Dry Goods',
-			image: 'drygoods.jpeg',
-			amt: 0,
-			expanded: false,
-			items: [
-				{
-					name: 'Rolled Oats',
-					quantity: 0,
-					quantityLabel: 'bag',
-					quantityLabelPlural: 'bags'
-				},
-				{
-					name: 'Brown Rice',
-					quantity: 0,
-					quantityLabel: 'bag',
-					quantityLabelPlural: 'bags'
-				}
+				]
+			},
+			{
+				id: 5,
+				label: 'Flowers',
+				image: 'flowers.jpeg',
+				amt: 0,
+				expanded: false,
+				items: [
+					{
+						id: 10,
+						name: 'Assortment',
+						quantity: 0,
+						unitLabelSingular: 'bunch',
+						unitLabelPlural: 'bunches',
+						unitCost: 8,
+						description: '',
+						ingredients: '',
+						producer: ''
+					}
+				]
+			},
+			{
+				id: 6,
+				label: 'Meat',
+				image: 'meat.jpeg',
+				amt: 0,
+				expanded: false,
+				items: [
+					{
+						id: 11,
+						name: 'Ground Beef',
+						quantity: 0,
+						unitLabelSingular: 'pound',
+						unitLabelPlural: 'pounds',
+						unitCost: 8,
+						description: '',
+						ingredients: '',
+						producer: ''
+					},
+					{
+						id: 12,
+						name: 'Hot Dogs',
+						quantity: 0,
+						unitLabelSingular: 'pack',
+						unitLabelPlural: 'packs',
+						unitCost: 8,
+						description: '',
+						ingredients: '',
+						producer: ''
+					}
 
-			]
-		},
-		{
-			label: 'Ferments',
-			image: 'ferments.jpeg',
-			amt: 1,
-			expanded: false,
-			items: [
-				{
-					name: 'Sauerkraut',
-					quantity: 0,
-					quantityLabel: 'jar',
-					quantityLabelPlural: 'jars'
-				},
-				{
-					name: 'Ruby Kraut',
-					quantity: 1,
-					quantityLabel: 'jar',
-					quantityLabelPlural: 'jars'
-				}
-
-			]
-		},
-		{
-			label: 'Flowers',
-			image: 'flowers.jpeg',
-			amt: 1,
-			expanded: false,
-			items: [
-				{
-					name: 'Assortment',
-					quantity: 1,
-					quantityLabel: 'bunch',
-					quantityLabelPlural: 'bunches'
-				}
-			]
-		},
-		{
-			label: 'Meat',
-			image: 'meat.jpeg',
-			amt: 1,
-			expanded: false,
-			items: [
-				{
-					name: 'Ground Beef',
-					quantity: 0,
-					quantityLabel: 'pound',
-					quantityLabelPlural: 'pounds'
-				},
-				{
-					name: 'Hot Dogs',
-					quantity: 1,
-					quantityLabel: 'pack',
-					quantityLabelPlural: 'packs'
-				}
-
-			]
-		},
-		{
-			label: 'Roots / Storage',
-			image: 'roots.jpeg',
-			amt: 0,
-			expanded: false,
-			items: [
-				{
-					name: 'Assortment',
-					quantity: 0,
-					quantityLabel: 'bunch',
-					quantityLabelPlural: 'bunches'
-				}
-			]
-		},
-		{
-			label: 'Sweets',
-			image: 'sweets.jpeg',
-			amt: 0,
-			expanded: false,
-			items: [
-				{
-					name: 'Honey',
-					quantity: 0,
-					quantityLabel: 'jar',
-					quantityLabelPlural: 'jars'
-				},
-				{
-					name: 'Jam',
-					quantity: 0,
-					quantityLabel: 'jar',
-					quantityLabelPlural: 'jars'
-				},
-				{
-					name: 'Maple Syrup',
-					quantity: 0,
-					quantityLabel: 'jar',
-					quantityLabelPlural: 'jars'
-				}
-			]
-		}
-	];
+				]
+			},
+			{
+				id: 7,
+				label: 'Roots / Storage',
+				image: 'roots.jpeg',
+				amt: 0,
+				expanded: false,
+				items: [
+					{
+						id: 13,
+						name: 'Assortment',
+						quantity: 0,
+						unitLabelSingular: 'bunch',
+						unitLabelPlural: 'bunches',
+						unitCost: 8,
+						description: '',
+						ingredients: '',
+						producer: ''
+					}
+				]
+			},
+			{
+				id: 8,
+				label: 'Sweets',
+				image: 'sweets.jpeg',
+				amt: 0,
+				expanded: false,
+				items: [
+					{
+						id: 14,
+						name: 'Honey',
+						quantity: 0,
+						unitLabelSingular: 'jar',
+						unitLabelPlural: 'jars',
+						unitCost: 5,
+						description: 'N/A',
+						ingredients: '',
+						producer: ''
+					},
+					{
+						id: 15,
+						name: 'Jam',
+						quantity: 0,
+						unitLabelSingular: 'jar',
+						unitLabelPlural: 'jars',
+						unitCost: 8,
+						description: '',
+						ingredients: '',
+						producer: ''
+					},
+					{
+						id: 16,
+						name: 'Maple Syrup',
+						quantity: 0,
+						unitLabelSingular: 'jar',
+						unitLabelPlural: 'jars',
+						unitCost: 8,
+						description: '',
+						ingredients: '',
+						producer: ''
+					}
+				]
+			}
+		],
+	}
 }
